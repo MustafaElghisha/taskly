76ec390b8b3686b30c6c6e2f79d7bcb18cd6f82a
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-14 11:44:52 +0300

    feat: add authenticated fetching

---
+++ src/features/auth/lib/auth/authenticatedFetch.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { getAccessToken, refreshAccessToken } from "./session";
+
+export async function authenticatedFetch(
+  input: string,
+  init: RequestInit = {},
+) {
+  let accessToken = await getAccessToken();
+
+  if (!accessToken) {
+    accessToken = await refreshAccessToken();
+  }
+
+  if (!accessToken) {
+    throw new Error("UNAUTHENTICATED");
+  }
+
+  let response = await fetch(input, {
+    ...init,
+    headers: {
+      ...init.headers,
+      Authorization: `Bearer ${accessToken}`,
+    },
+    cache: "no-store",
+  });
+
+  if (response.status !== 401) {
+    return response;
+  }
+
+  accessToken = await refreshAccessToken();
+
+  if (!accessToken) {
+    throw new Error("UNAUTHENTICATED");
+  }
+
+  response = await fetch(input, {
+    ...init,
+    headers: {
+      ...init.headers,
+      Authorization: `Bearer ${accessToken}`,
+    },
+    cache: "no-store",
+  });
+
+  return response;
+}
