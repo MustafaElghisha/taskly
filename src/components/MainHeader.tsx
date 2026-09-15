71605d37684a7b5012781d6abeaf61304b700ddd
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-15 20:50:41 +0300

    feat: add main header component

---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,40 @@
+import BurgerMenuIcon from "@/assets/icons/BurgerMenuIcon.svg";
+import UserInfo from "./UserInfo";
+import { Separator } from "./ui/Separator";
+import { UserData } from "@/lib/getUserData";
+
+type MainHeaderProps = {
+  toggleSidebar: () => void;
+  isSidebarOpen: boolean;
+  user: UserData;
+};
+
+export default function MainHeader({
+  toggleSidebar,
+  isSidebarOpen,
+  user,
+}: MainHeaderProps) {
+  return (
+    <div>
+      <header className="ml-auto flex w-full items-center justify-between px-6 py-5 sm:justify-end sm:px-8 sm:py-3">
+        <div className="flex items-center gap-4 sm:hidden">
+          <button
+            type="button"
+            aria-label="Main Navigation Menu"
+            aria-expanded={isSidebarOpen}
+            aria-controls="main-navigation"
+            className="cursor-pointer"
+            onClick={toggleSidebar}
+          >
+            <BurgerMenuIcon aria-hidden="true" />
+          </button>
+          <span className="text-xl leading-7 font-bold tracking-tight text-slate-800">
+            TASKLY
+          </span>
+        </div>
+        <UserInfo name={user.name} jobTitle={user.jobTitle} />
+      </header>
+      <Separator />
+    </div>
+  );
+}
