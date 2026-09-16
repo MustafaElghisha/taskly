978928a7673b9fdf6e06971756d6b47602f4b878
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-16 07:44:08 +0300

    feat: add zod schema for create project

---
+++ src/features/project/schemas/CreateProjectSchema.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const CreateProjectSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .nonempty("Project title is required.")
+    .min(3, { message: "Project title must be at least 3 characters." })
+    .max(100, "Project title must be at most 100 characters."),
+  description: z
+    .string()
+    .max(500, {
+      message: "Project description must be at most 500 characters",
+    })
+    .optional(),
+});
+
+export type CreateProjectSchemaType = z.infer<typeof CreateProjectSchema>;
