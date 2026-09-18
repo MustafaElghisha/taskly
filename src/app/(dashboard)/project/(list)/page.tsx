e1045cde44bd31ed9801c81fb28bf9dbd7f0f4d4
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-18 15:13:10 +0300

    refactor: extract some ui components into its own files

--- src/app/(dashboard)/project/(list)/page.tsx
+++ src/app/(dashboard)/project/(list)/page.tsx
@@ -1,73 +1,57 @@
+import Link from "next/link";
+
+import AddIcon from "@/assets/icons/AddIcon.svg";
 import { buttonVariants } from "@/components/ui/Button";
 import { getProjects } from "@/features/project/actions/getProjects";
 import ProjectList from "@/features/project/components/ProjectList";
-import NoProjectsIcon from "@/assets/icons/NoProjectsIcon.svg";
-import AddIcon from "@/assets/icons/AddIcon.svg";
-import Link from "next/link";
+import EmptyProjects from "@/features/project/components/EmptyProjects";
+import ProjectPagination from "@/features/project/components/ProjectPagination";
 
 export default async function ProjectPage() {
   const projects = await getProjects();
 
+  if (!projects.length) return <EmptyProjects />;
+
   return (
-    <>
-      {projects.length ? (
-        <div className="p-8">
-          <div className="flex flex-wrap items-end justify-between gap-x-20 gap-y-4">
-            <div className="flex flex-col gap-1">
-              <h1 className="text-2xl leading-8 font-semibold tracking-tight text-slate-800 sm:text-3xl sm:leading-9">
-                Projects
-              </h1>
-              <p className="leading-6 text-slate-600">
-                Manage and curate your projects
-              </p>
-            </div>
-            <Link
-              href="/project/add"
-              className={buttonVariants({
-                className: "hidden rounded-xs px-6 font-medium sm:block",
-                variant: "primary",
-              })}
-            >
-              Create New Project
-            </Link>
-          </div>
-          <div className="py-10">
-            <ProjectList projects={projects} />
-          </div>
-          <Link
-            href="/project/add"
-            className={buttonVariants({
-              variant: "primary",
-              className:
-                "fixed right-6 bottom-20 rounded-xl p-5.25! leading-0 sm:hidden",
-            })}
-          >
-            <AddIcon />
-          </Link>
+    <div className="px-5 py-4 sm:p-8">
+      <div className="flex flex-wrap items-end justify-between gap-x-20 gap-y-4">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl leading-8 font-semibold tracking-tight text-slate-800 sm:text-3xl sm:leading-9">
+            Projects
+          </h1>
+          <p className="leading-6 text-slate-600">
+            Manage and curate your projects
+          </p>
         </div>
-      ) : (
-        <div className="flex h-full flex-col items-center justify-center gap-11 px-8">
-          <NoProjectsIcon />
-          <div className="flex flex-col items-center justify-center gap-4 text-center">
-            <h1 className="text-4xl leading-10 font-semibold tracking-tighter text-slate-800">
-              No Projects
-            </h1>
-            <p className="max-w-[40ch] text-lg leading-7.5 text-slate-600">
-              You don’t have any projects yet. Start by defining your first
-              architectural workspace to begin tracking tasks and epics.
-            </p>
-          </div>
-          <Link
-            href="/project/add"
-            className={buttonVariants({
-              className: "rounded-sm",
-              variant: "primary",
-            })}
-          >
-            Create New Project
-          </Link>
-        </div>
-      )}
-    </>
+        <Link
+          href="/project/add"
+          className={buttonVariants({
+            className: "hidden rounded-xs px-6 font-medium sm:block",
+            variant: "primary",
+          })}
+        >
+          Create New Project
+        </Link>
+      </div>
+
+      <div className="py-6 sm:pt-10 sm:pb-17.5">
+        <ProjectList projects={projects} />
+      </div>
+
+      <div className="hidden py-10 sm:block">
+        <ProjectPagination />
+      </div>
+
+      <Link
+        href="/project/add"
+        className={buttonVariants({
+          variant: "primary",
+          className:
+            "fixed right-6 bottom-20 rounded-xl p-5.25! leading-0 sm:hidden",
+        })}
+      >
+        <AddIcon />
+      </Link>
+    </div>
   );
 }
