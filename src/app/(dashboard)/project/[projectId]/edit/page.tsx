f0d6e7655d27f2d9c67b3fc864afc1f2188d3217
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-22 18:39:31 +0300

    feat: add edit project page

--- src/app/(dashboard)/project/[projectId]/edit/page.tsx
+++ src/app/(dashboard)/project/[projectId]/edit/page.tsx
@@ -1,7 +1,20 @@
-export default function EditPage() {
+import { getProject } from "@/features/project/actions/getProject";
+import EditProjectForm from "@/features/project/components/EditProjectForm";
+
+export default async function EditPage({
+  params,
+}: {
+  params: Promise<{ projectId: string }>;
+}) {
+  const { projectId } = await params;
+  const project = await getProject(projectId);
+
   return (
-    <div className="p-8">
-      <h1>EDIT PAGE</h1>
+    <div className="px-6 py-8 sm:px-8 sm:pt-4 sm:pb-6">
+      <h1 className="hidden text-4xl leading-10 font-semibold tracking-tight text-slate-800 sm:block">
+        Edit Project
+      </h1>
+      <EditProjectForm project={project} />
     </div>
   );
 }
