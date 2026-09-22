6339d2e419c1e32e7dfab955e608f50bbc617926
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-22 21:59:34 +0300

    refactor: extracted project form component and use it in edit and create project components

--- src/features/project/components/EditProjectForm.tsx
+++ src/features/project/components/EditProjectForm.tsx
@@ -1,128 +1,16 @@
 "use client";
 
-import InitializeIcon from "@/assets/icons/InitializeIcon.svg";
-import TipIcon from "@/assets/icons/TipIcon.svg";
-import {
-  Field,
-  FieldDescription,
-  FieldError,
-  FieldLabel,
-} from "@/components/ui/Field";
-import Input from "@/components/ui/Input";
-import { Textarea } from "@/components/ui/Textarea";
-import { Separator } from "@/components/ui/Separator";
-import Button, { buttonVariants } from "@/components/ui/Button";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
 import { useEditProject } from "../hooks/useEditProject";
 import { Project } from "@/types";
+import ProjectForm from "./ProjectForm";
 
 export default function EditProjectForm({ project }: { project: Project }) {
-  const { register, errors, onSubmit, isSubmitting, watchDescription } =
-    useEditProject(project);
-
   return (
-    <div className="mx-auto max-w-2xl">
-      <div className="sm:shadow-form-container flex flex-col gap-8 rounded-t-lg sm:mt-11 sm:gap-0 sm:bg-white">
-        <div className="flex items-center gap-4 sm:p-8">
-          <div className="bg-primary-container/10 hidden items-center justify-center rounded-sm p-4 sm:flex">
-            <InitializeIcon />
-          </div>
-          <div>
-            <h2 className="pb-1 text-2xl leading-8 font-semibold text-slate-800 sm:pb-0">
-              Edit Project
-            </h2>
-            <p className="text-sm leading-5 text-slate-500">
-              Define the scope and foundational details of your project.
-            </p>
-          </div>
-        </div>
-
-        <Separator className="hidden bg-black/5 sm:block" />
-
-        <form
-          className="flex flex-col gap-8 sm:p-8 sm:pb-12"
-          onSubmit={onSubmit}
-        >
-          <Field>
-            <FieldLabel htmlFor="project-name">
-              Project TITLE <span className="text-error">*</span>
-            </FieldLabel>
-            <Input
-              id="project-name"
-              type="text"
-              placeholder="e.g. Tasks Management project"
-              aria-invalid={errors.name ? "true" : "false"}
-              {...register("name")}
-            />
-            {errors.name && <FieldError>{errors.name.message}</FieldError>}
-          </Field>
-          <Field>
-            <div className="flex items-center justify-between">
-              <FieldLabel htmlFor="project-description">Description</FieldLabel>
-              <span className="text-2xs hidden leading-4 text-slate-500/60 sm:inline">
-                Optional
-              </span>
-            </div>
-
-            <Textarea
-              rows={6}
-              id="project-description"
-              placeholder="Provide a high-level overview of the project's architectural objectives and key milestones..."
-              aria-invalid={errors.description ? "true" : "false"}
-              {...register("description")}
-            />
-            <FieldDescription
-              className={cn(
-                "text-3xs self-end leading-4 font-medium",
-                watchDescription?.length > 500 && "text-error",
-              )}
-            >
-              {watchDescription?.length} / 500
-              <span className="hidden ps-0.5 sm:inline">characters</span>
-            </FieldDescription>
-          </Field>
-          <div className="flex flex-col-reverse items-center justify-between gap-y-7 pt-4 sm:flex-row">
-            <Link
-              href={"/project"}
-              className={buttonVariants({ variant: "ghost" })}
-            >
-              Back
-            </Link>
-            <Button
-              type="submit"
-              disabled={isSubmitting}
-              variant={"primary"}
-              className="primary-button-shadow w-full font-bold sm:w-fit sm:rounded-sm sm:text-sm sm:leading-5"
-            >
-              {isSubmitting ? "Saving..." : "Save Changes"}
-            </Button>
-          </div>
-          <Field className="flex items-center">
-            {errors.root && (
-              <FieldError className="text-center">
-                {errors.root.message}
-              </FieldError>
-            )}
-          </Field>
-        </form>
-      </div>
-
-      <div className="bg-surface-low -mt-1 flex flex-col gap-2 rounded-lg p-6 sm:mb-11 sm:flex-row sm:items-baseline sm:rounded-t-none">
-        <div className="hidden sm:block">
-          <TipIcon />
-        </div>
-        <span className="text-xs leading-5 font-bold text-slate-500 sm:hidden">
-          Pro Tip
-        </span>
-        <p className="text-xs text-slate-500">
-          <span className="hidden pe-0.5 text-xs leading-5 font-bold text-slate-500 sm:inline">
-            Pro Tip:
-          </span>
-          You can invite project members and assign epics immediately after the
-          initial creation process.
-        </p>
-      </div>
-    </div>
+    <ProjectForm
+      formTitle="Edit Project"
+      submitLabel="Save Changes"
+      submittingLabel="Saving..."
+      {...useEditProject(project)}
+    />
   );
 }
