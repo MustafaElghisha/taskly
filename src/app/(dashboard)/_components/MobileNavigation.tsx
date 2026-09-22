0725643755ac8cf828dc421bf22433e18f0d89c8
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-22 11:14:35 +0300

    refactor: use route constants in navigation components

--- src/app/(dashboard)/_components/MobileNavigation.tsx
+++ src/app/(dashboard)/_components/MobileNavigation.tsx
@@ -1,64 +1,59 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-
-import EpicsIcon from "@/assets/icons/EpicsIcon.svg";
-import TasksIcon from "@/assets/icons/TasksIcon.svg";
-import MembersIcon from "@/assets/icons/MembersIcon.svg";
-import DetailsIcon from "@/assets/icons/DetailsIcon.svg";
+import { useParams, usePathname } from "next/navigation";
 import ProjectsIcon from "@/assets/icons/ProjectsIcon.svg";
-import { usePathname } from "next/navigation";
-
-const PROJECT_ITEMS = [
-  {
-    title: "Epics",
-    url: "/",
-    Icon: EpicsIcon,
-  },
-  {
-    title: "Tasks",
-    url: "/",
-    Icon: TasksIcon,
-  },
-  {
-    title: "Projects",
-    url: "/project",
-    Icon: ProjectsIcon,
-  },
-  {
-    title: "Members",
-    url: "/",
-    Icon: MembersIcon,
-  },
-  {
-    title: "Details",
-    url: "/",
-    Icon: DetailsIcon,
-  },
-];
+import { ACTIVE_PROJECT_ROUTES } from "../_constants/routes";
 
 export default function MobileNavigation() {
-  const pathName = usePathname();
+  const pathname = usePathname();
+  const { projectId } = useParams();
+
+  const activeProjectRoute = pathname.split("/")[3];
+
   return (
     <div className="bg-surface-low block sm:hidden">
       <nav className="px-7 py-3.75">
-        <ul className="flex items-center justify-between">
-          {PROJECT_ITEMS.map(({ title, url, Icon }) => (
-            <li key={title}>
+        {!activeProjectRoute ? (
+          <ul className="flex items-center justify-center">
+            <li>
               <Link
-                href={url}
+                href={"/project"}
                 className={cn(
-                  "flex flex-col items-center justify-center gap-0.5",
-                  pathName === url
+                  "flex flex-col items-center justify-center gap-0.5 capitalize",
+                  pathname.endsWith("/project")
                     ? "text-primary font-semibold"
                     : "text-slate-800",
                 )}
               >
-                <Icon />
-                <span className="text-label-xs leading-4">{title}</span>
+                <ProjectsIcon />
+                <span className="text-3xs leading-4">projects</span>
               </Link>
             </li>
-          ))}
-        </ul>
+          </ul>
+        ) : (
+          <ul className="flex items-center justify-between gap-6">
+            {ACTIVE_PROJECT_ROUTES.map(({ title, segment, Icon }) => (
+              <li key={title} className="flex-1">
+                <Link
+                  href={
+                    title === "projects"
+                      ? `/${segment}`
+                      : `/project/${projectId}/${segment}`
+                  }
+                  className={cn(
+                    "flex flex-col items-center justify-center gap-0.5 capitalize",
+                    activeProjectRoute === segment
+                      ? "text-primary font-semibold"
+                      : "text-slate-800",
+                  )}
+                >
+                  <Icon />
+                  <span className="text-3xs leading-4">{title}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </nav>
     </div>
   );
--- src/app/(dashboard)/_components/ProjectMenu.tsx
+++ src/app/(dashboard)/_components/ProjectMenu.tsx
@@ -1,47 +1,33 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-
-import EpicsIcon from "@/assets/icons/EpicsIcon.svg";
-import TasksIcon from "@/assets/icons/TasksIcon.svg";
-import MembersIcon from "@/assets/icons/MembersIcon.svg";
-import DetailsIcon from "@/assets/icons/DetailsIcon.svg";
-
-const PROJECT_ITEMS = [
-  {
-    title: "Epics",
-    url: "/",
-    Icon: EpicsIcon,
-  },
-  {
-    title: "Tasks",
-    url: "/",
-    Icon: TasksIcon,
-  },
-  {
-    title: "Members",
-    url: "/",
-    Icon: MembersIcon,
-  },
-  {
-    title: "Details",
-    url: "/",
-    Icon: DetailsIcon,
-  },
-];
+import { useParams, usePathname } from "next/navigation";
+import { ACTIVE_PROJECT_ROUTES } from "../_constants/routes";
 
 export default function ProjectMenu({ className }: { className?: string }) {
+  const pathName = usePathname();
+  const { projectId } = useParams();
+
   return (
     <ul className={cn("flex flex-col gap-1 p-2", className)}>
-      {PROJECT_ITEMS.map(({ title, url, Icon }) => (
-        <li key={title}>
-          <Link href={url} className="flex items-center gap-3 p-3">
-            <Icon />
-            <span className="text-sm leading-5 font-medium text-slate-800">
-              {title}
-            </span>
-          </Link>
-        </li>
-      ))}
+      {ACTIVE_PROJECT_ROUTES.map(({ title, segment, Icon }) => {
+        if (segment === "project") return null;
+        return (
+          <li key={title}>
+            <Link
+              href={`/project/${projectId}/${segment}`}
+              className={cn(
+                "flex items-center gap-3 rounded-full p-3 capitalize",
+                pathName.includes(segment) && "bg-surface-low",
+              )}
+            >
+              <Icon />
+              <span className="text-sm leading-5 font-medium text-slate-800">
+                {title}
+              </span>
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
