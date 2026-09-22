81188f4ed99695724ab223cf032f7719674ca5e9
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-22 15:00:27 +0300

    feat: add edit link to the project card

--- src/features/project/components/ProjectCard.tsx
+++ src/features/project/components/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import EpicsIcon from "@/assets/icons/EpicsIcon.svg";
 import TasksIcon from "@/assets/icons/TasksIcon.svg";
 import MembersIcon from "@/assets/icons/MembersIcon.svg";
+import EditIcon from "@/assets/icons/EditIcon.svg";
 import { Project } from "@/types";
 import Link from "next/link";
 
@@ -12,9 +13,26 @@ const formatDate = (date: string) =>
   }).format(new Date(date));
 
 const PROJECT_NAV_ITEMS = [
-  { label: "Epics", Icon: EpicsIcon },
-  { label: "Tasks", Icon: TasksIcon },
-  { label: "Members", Icon: MembersIcon },
+  {
+    title: "epics",
+    segment: "epics",
+    Icon: EpicsIcon,
+  },
+  {
+    title: "tasks",
+    segment: "tasks",
+    Icon: TasksIcon,
+  },
+  {
+    title: "members",
+    segment: "members",
+    Icon: MembersIcon,
+  },
+  {
+    title: "edit",
+    segment: "edit",
+    Icon: EditIcon,
+  },
 ];
 
 export default function ProjectCard({
@@ -24,40 +42,40 @@ export default function ProjectCard({
   created_at,
 }: Project) {
   return (
-    <li>
-      <Link
-        href={`/project/${id}/epics`}
-        className="flex h-full flex-col justify-between gap-8.5 rounded-lg bg-white p-6"
-      >
-        <div className="flex flex-1 flex-col justify-between gap-6">
-          <div className="flex flex-1 flex-col justify-between gap-2">
-            <h2 className="line-clamp-2 text-lg leading-7 font-medium text-slate-800">
-              {name}
-            </h2>
-            <p className="line-clamp-3 text-sm leading-5.5 hyphens-auto text-slate-600">
-              {description}
-            </p>
-          </div>
-          <ul className="flex items-center justify-between">
-            {PROJECT_NAV_ITEMS.map(({ label, Icon }) => (
-              <li key={label} className="text-primary flex gap-0.5">
+    <li className="flex h-full flex-col justify-between gap-8.5 rounded-lg bg-white p-6">
+      <div className="flex flex-1 flex-col justify-between gap-6">
+        <div className="flex flex-1 flex-col justify-between gap-2">
+          <h2 className="line-clamp-2 text-lg leading-7 font-medium text-slate-800">
+            {name}
+          </h2>
+          <p className="line-clamp-3 text-sm leading-5.5 hyphens-auto text-slate-600">
+            {description}
+          </p>
+        </div>
+        <ul className="flex flex-wrap items-center justify-between gap-1">
+          {PROJECT_NAV_ITEMS.map(({ title, Icon }) => (
+            <li key={title}>
+              <Link
+                href={`/project/${id}/${title}`}
+                className="text-primary flex items-center gap-0.5 capitalize"
+              >
                 <Icon />
                 <span className="text-3xs leading-4 font-semibold">
-                  {label}
+                  {title}
                 </span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="flex items-center justify-between">
-          <span className="text-2xs leading-4 font-bold tracking-tighter text-slate-400">
-            CREATED AT
-          </span>
-          <time className="text-sm leading-5 font-medium text-slate-600">
-            {formatDate(created_at)}
-          </time>
-        </div>
-      </Link>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex items-center justify-between">
+        <span className="text-2xs leading-4 font-bold tracking-tighter text-slate-400">
+          CREATED AT
+        </span>
+        <time className="text-sm leading-5 font-medium text-slate-600">
+          {formatDate(created_at)}
+        </time>
+      </div>
     </li>
   );
 }
--- src/features/project/components/ProjectList.tsx
+++ src/features/project/components/ProjectList.tsx
@@ -4,9 +4,9 @@ import ProjectCard from "./ProjectCard";
 
 export default function ProjectList({ projects }: { projects: Project[] }) {
   return (
-    <ol className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4 sm:gap-6">
+    <ol className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-4 sm:gap-6">
       {projects?.map((project) => (
-        <ProjectCard key={project.created_at} {...project} />
+        <ProjectCard key={project.id} {...project} />
       ))}
       <AddProjectCard />
     </ol>
