c576912e7a52c95a1cc4d6ec2bf5073b3cd03745
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-24 18:14:49 +0300

    feat: add forgot password page

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+import ForgotPasswordForm from "@/features/auth/components/ForgotPasswordForm";
+
+export default function ForgotPasswordPage() {
+  return (
+    <div className="px-6 py-13 sm:py-15">
+      <ForgotPasswordForm />
+    </div>
+  );
+}
