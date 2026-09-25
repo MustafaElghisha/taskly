dff2d1326dc2b9a647b7610aa3faed73834640b2
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-25 17:58:51 +0300

    feat: add reset password page

---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,9 @@
+import ResetPasswordForm from "@/features/auth/components/ResetPasswordForm";
+
+export default function ResetPasswordPage() {
+  return (
+    <div className="p-6 sm:p-23">
+      <ResetPasswordForm />
+    </div>
+  );
+}
