5d57ba22ba293ed6744fa6d232c2447879dcd607
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-25 17:53:54 +0300

    feat: add reset password custom hook

---
+++ src/features/auth/hooks/useResetPassword.ts
@@ -0,0 +1,60 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import {
+  ResetPasswordInput,
+  resetPasswordInputSchema,
+} from "../schemas/resetPasswordSchema";
+import { resetPassword } from "../actions/resetPassword";
+
+const useResetPassword = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setError,
+    clearErrors,
+    getFieldState,
+    trigger,
+    formState: { errors, isSubmitting },
+  } = useForm<ResetPasswordInput>({
+    resolver: zodResolver(resetPasswordInputSchema),
+    mode: "onTouched",
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const watchPassword = watch("password");
+
+  useEffect(() => {
+    if (getFieldState("confirmPassword").isDirty) {
+      trigger("confirmPassword");
+    }
+  }, [watchPassword, getFieldState, trigger]);
+
+  const router = useRouter();
+
+  const onSubmit = handleSubmit(async (data) => {
+    clearErrors("root");
+
+    const result = await resetPassword(data);
+
+    if (!result.success) {
+      setError("root", {
+        message: result.message,
+      });
+      return;
+    }
+
+    setTimeout(() => {
+      router.push("/login");
+    }, 3000);
+  });
+
+  return { register, errors, isSubmitting, onSubmit, watchPassword };
+};
+
+export { useResetPassword };
