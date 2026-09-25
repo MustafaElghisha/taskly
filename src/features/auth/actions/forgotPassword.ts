073717c154ecf7d50ccba5016d2ab8f81e9b2e95
MustafaElghisha <mustafaelghisha@gmail.com>
2026-09-25 17:52:17 +0300

    refactor: make forgot password a server action and remove try catch block

--- src/features/auth/actions/forgotPassword.ts
+++ src/features/auth/actions/forgotPassword.ts
@@ -1,29 +1,21 @@
+"use server";
+
 import { ForgotPasswordInput } from "../schemas/forgotPasswordSchema";
 
 export async function forgotPassword({ email }: ForgotPasswordInput) {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/recover`,
-      {
-        method: "POST",
-        headers: {
-          apikey: process.env.NEXT_PUBLIC_SUPABASE_API_KEY!,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-        }),
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/recover`,
+    {
+      method: "POST",
+      headers: {
+        apikey: process.env.NEXT_PUBLIC_SUPABASE_API_KEY!,
+        "Content-Type": "application/json",
       },
-    );
+      body: JSON.stringify({ email }),
+    },
+  );
 
-    if (!response.ok) {
-      throw new Error("REQUEST_FAILED");
-    }
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    } else {
-      throw new Error("Something went wrong. Please try again later.");
-    }
+  if (!response.ok) {
+    throw new Error("REQUEST_FAILED");
   }
 }
